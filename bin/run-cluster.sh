499d15d4e80668076b7adea597f763b0ad67954d
giuse-to <pataniag@gmail.com>
2017-05-26 16:04:21 +0200

    reviewed pointed paths

--- bin/run-cluster.sh
+++ bin/run-cluster.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/sh
 
 CWD=`dirname $0`
-source ${CWD}/conf/galera-tools.conf
-source ${CWD}/lib/*
+source ${CWD}/../conf/galera-tools.conf
+source ${CWD}/../lib/*
 
+#write_to_log ${LOGFILE} "the script is working" ${INFO}
+	
 exit 0
